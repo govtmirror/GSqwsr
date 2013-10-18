6cba680f49facdc00723b3d39835e5a0fd9e75f1
Laura DeCicco <ldecicco@usgs.gov>
2013-10-18 15:36:01 -0500

    Added AIC output to graph.

--- R/analyzeSteps.R
+++ R/analyzeSteps.R
@@ -22,11 +22,11 @@
 #' returnPrelim <- prelimModelDev(DT,response,kitchenSink)
 #' steps <- returnPrelim$steps
 #' analyzeSteps(steps,response,siteINFO)
-analyzeSteps <- function(steps, responseVariable, siteINFO, xCorner = 0.65, yCorner=0.3){
+analyzeSteps <- function(steps, responseVariable, siteINFO, xCorner = 0.6, yCorner=0.35){
   
   parOriginal <- par(no.readonly = TRUE)
   
-  par(mar=c(5,5,5,5))
+  par(mar=c(5,12,5,5))
   
   with(steps, plot(Correlation, xlab="Step", ylab="Corrolation, Slope, RMSE", 
                    ylim=c(0,1), axes=FALSE, pch=20, col="black",type="o"))
@@ -34,16 +34,21 @@ analyzeSteps <- function(steps, responseVariable, siteINFO, xCorner = 0.65, yCor
   with(steps, points(Res.St.Error, pch=20, col="blue",type="o"))
   axis(2,  ylim=c(0,1))
   axis(1,  xlim=c(1,length(steps)))
-  par(new=T)
+  par(new=TRUE)
   with(steps, plot(PRESS, axes=FALSE, xlab="", ylab= "",
                    pch=20, col="forestgreen", ylim=c(0,max(PRESS,na.rm=TRUE)),type="o"))
   axis(4,  ylim=c(0,max(steps$PRESS,na.rm=TRUE)))
   mtext("PRESS", side=4, line=2)
+  par(new=TRUE)
+  with(steps, plot(AIC, axes=FALSE, xlab="", ylab= "",
+                   pch=20, col="grey", ylim=c(min(AIC,na.rm=TRUE),max(AIC,na.rm=TRUE)),type="o"))
+  axis(2,  ylim=c(min(steps$AIC,na.rm=TRUE),max(steps$AIC,na.rm=TRUE)),line=5)
+  mtext("'AIC'", side=2, line=7)
   box()
   x1 <- grconvertX(xCorner, from="npc", to="user")
   y1 <- grconvertY(yCorner, from="npc", to="user")  
-  legend(x1, y1, c("Correlation", "Slope", "RMSE", "PRESS"), 
-         pch=c(20, 20, 20, 20),col=c("black", "red","blue","forestgreen"))
+  legend(x1, y1, c("Correlation", "Slope", "RMSE", "PRESS","'AIC'"), 
+         pch=c(20, 20, 20, 20,20),col=c("black", "red","blue","forestgreen","grey"),bg="white")
   
   prettyName <- simpleCap(siteINFO$station.nm)
   prettyName <- gsub("Wi", "WI",prettyName) #Consider other states.
