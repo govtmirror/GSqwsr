03509f5f58d22e5cbaf4f8a3a2f0f67dfca6a966
Laura DeCicco <ldecicco@usgs.gov>
2013-12-30 13:31:50 -0600

    Added support for time (not just days).

--- R/getDecYear.R
+++ R/getDecYear.R
@@ -13,10 +13,18 @@
 #'decYear2 <- getDecYear(date2)
 getDecYear <- function(dateTime){
   dateTime <- as.POSIXlt(dateTime)
-  Julian <- as.numeric(julian(dateTime,origin=as.Date("1850-01-01")))
-  Month <- dateTime$mon + 1
-  Day <- dateTime$yday + 1
+#   Julian <- as.numeric(julian(dateTime,origin=as.Date("1850-01-01")))
+#   Month <- dateTime$mon + 1
+#   Day <- dateTime$yday + 1
   year <- dateTime$year + 1900
-  decYear <- year + (Day -0.5)/366
+#   hour <- dateTime$hour
+#   minute <- dateTime$min
+#   decYear <- year + (Day -0.5)/366 + (hour)/(24*366) + minute/(24*60*366)
+  jan1 <- as.POSIXlt(paste(year,"-01-01",sep=""))
+  jan1NextYear <- as.POSIXlt(paste(year+1,"-01-01",sep=""))
+  decimal <- as.numeric(difftime(dateTime, jan1, units = "secs"))
+  decimal <- decimal/as.numeric(difftime(jan1NextYear, jan1, units = "secs"))
+  
+  decYear <- year + decimal
   return(decYear)
 }
