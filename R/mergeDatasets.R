f20e6dd00c77c361f861606749c4be8cb84c4838
Laura DeCicco <ldecicco@usgs.gov>
2014-01-07 16:23:06 -0600

    Updated documentation, changed argument from QW to localQW to avoid enviornment confusion.

--- R/mergeDatasets.R
+++ R/mergeDatasets.R
@@ -1,32 +1,37 @@
-#'mergeDatasets
+#'Creates a dataframe of merged datasets.
 #'
-#'Creates a list of merged datasets, along with updating the QWcodes dataframe.
+#'Creates a list of merged datasets, along with updating the QWcodes dataframe. The function looks for
+#'qw objects in the QW dataframe, cross-references them with the QWcodes table, and merges those
+#'columns with the nearest UV (unit value) data. The function will merge the UV data that is nearest
+#'to the QW data, see \code{?mergeNearest} for more options on specifying the acceptable time gap between
+#'the two datasets. 
 #'
-#'@param QW dataframe water quality dataset
-#'@param UV dataframe unit value dataset
+#'@param localQW dataframe water quality dataset. Should have qw objects, 
+#'along with a column called "datetime" in POSIXct.
+#'@param localUV dataframe unit value dataset. Should have one column called "datetime" in POSIXct.
 #'@param QWcodes dataframe
-#'@param max.diff string default is "2 hours"
+#'@param max.diff string default is "2 hours". See \code{?mergeNearest} for more options.
 #'@return retList list including the DT list and updated QWcodes
 #'@keywords merge datasets
 #'@export
 #'@examples
-#' QW <- QW
-#' UV <- UV
-#' QWcodes <- QWcodes
+#' UV <- StLouisUV
+#' QW <- StLouisQW
+#' QWcodes <- StLouisQWcodes
 #' mergeReturn <- mergeDatasets(QW, UV, QWcodes)
 #' DTComplete <- mergeReturn$DTComplete
 #' QWcodes <- mergeReturn$QWcodes
-mergeDatasets <- function(QW, UV, QWcodes,max.diff="2 hours"){
+mergeDatasets <- function(localQW, localUV, QWcodes, max.diff="2 hours"){
 
-  namesToCheck <- getPredictVariables(names(UV))
+  namesToCheck <- getPredictVariables(names(localUV))
 
   ColName <- ""
-  index <- as.numeric(which(sapply(QW, function(x) class(x)) == "qw"))
+  index <- as.numeric(which(sapply(localQW, function(x) class(x)) == "qw"))
   
   for (i in 1:nrow(QWcodes)){
-    for(j in names(QW)[index]){ 
-      #     analyte.name <- unique(QW[,j]@analyte.name[!is.na(QW[,j]@analyte.name)])
-      pcode <- unique(QW[,j]@unique.code[!is.na(QW[,j]@unique.code)])
+    for(j in names(localQW)[index]){ 
+      #     analyte.name <- unique(localQW[,j]@analyte.name[!is.na(localQW[,j]@analyte.name)])
+      pcode <- unique(localQW[,j]@unique.code[!is.na(localQW[,j]@unique.code)])
       if (pcode == QWcodes$parameter_cd[i]){
         ColName <- j
         break
@@ -36,9 +41,9 @@ mergeDatasets <- function(QW, UV, QWcodes,max.diff="2 hours"){
   }
     
   for (j in namesToCheck){
-    subUV <- UV[c("datetime", j)]
+    subUV <- localUV[c("datetime", j)]
     subUV <- na.omit(subUV)
-    DT <- mergeNearest(QW["datetime"], dates.left="datetime", all.left=TRUE,
+    DT <- mergeNearest(localQW["datetime"], dates.left="datetime", all.left=TRUE,
                               right=subUV, dates.right="datetime", max.diff=max.diff)
     DT$datetime.right <- NULL
     if (namesToCheck[1] == j){
@@ -50,7 +55,7 @@ mergeDatasets <- function(QW, UV, QWcodes,max.diff="2 hours"){
   
   colnames(DTmerged) <- c("datetime",colnames(DTmerged)[-1])
   
-  DT_single <- merge(DTmerged,QW,by=("datetime"))
+  DT_single <- merge(DTmerged,localQW,by=("datetime"))
   
   DT_single$decYear <- getDecYear(DT_single$datetime)
   DT_single$sinDY <- sin(DT_single$decYear*2*pi)
